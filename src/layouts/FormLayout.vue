ad1e138547b3193e63f40cf737a4dea1521a9525
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-22 18:26:44 +0400

    feat: define layouts and not found page

---
+++ src/layouts/FormLayout.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Form Layout</h1>
+  <RouterView />
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import FormLayout from '@/layouts/FormLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'defaultLayout',
+      component: DefaultLayout,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: HomeView
+        }
+      ]
+    },
+    {
+      path: '/createBlog',
+      name: 'formLayout',
+      component: FormLayout
+    },
+    {
+      path: '/:notFound(.*)*',
+      name: 'notFound',
+      component: NotFoundView
     }
   ]
 })
