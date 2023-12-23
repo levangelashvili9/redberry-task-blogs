019bd581e823758b3e60cbbe0872993b7f3c715a
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-23 14:25:42 +0400

    feat: add reusable button

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { cva } from 'class-variance-authority'
+
+type IProps = {
+  variant: 'primary' | 'secondary'
+  disabled?: boolean
+}
+
+const props = defineProps<IProps>()
+
+const buttonClass = computed(() => {
+  return cva(
+    'inline-flex items-center justify-center text-white text-sm rounded min-h-10 px-5 py-[0.625rem] font-medium rounded-lg hover:bg-blue-500',
+    {
+      variants: {
+        variant: {
+          primary: 'bg-[#5D37F3]',
+          secondary: 'bg-yellow-600'
+        },
+        disabled: {
+          true: '!bg-gray-100 !text-gray-400 cursor-not-allowed'
+        }
+      }
+    }
+  )({
+    variant: props.variant,
+    disabled: props.disabled
+  })
+})
+</script>
+
+<template>
+  <button :class="buttonClass">
+    <slot />
+  </button>
+</template>
