ae64bdc476940220ebb16e71d9eb77c7dd27ba7f
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-23 23:12:54 +0400

    style: use reusable color instead of hard-coded

--- src/components/home/HomeHero.vue
+++ src/components/home/HomeHero.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex justify-between items-center px-20 mb-16">
-    <h1 class="text-[#1A1A1F] text-[4rem] font-bold">ბლოგი</h1>
+    <h2 class="text-primary text-[4rem] font-bold">ბლოგი</h2>
     <img src="../../assets/images/blog-hero.png" alt="blog hero image" width="624" height="200" />
   </section>
 </template>
