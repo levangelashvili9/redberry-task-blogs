e315bee936905ac3c8caeda5025cb876f372dede
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-23 23:14:24 +0400

    style: change style variants

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -11,12 +11,12 @@ const props = defineProps<IProps>()
 
 const buttonClass = computed(() => {
   return cva(
-    'inline-flex items-center justify-center text-white text-sm rounded min-h-10 px-5 py-[0.625rem] font-medium rounded-lg hover:bg-blue-500',
+    'inline-flex items-center justify-center bg-[#5D37F3] text-white text-sm rounded min-h-10 px-5 py-[0.625rem] font-medium rounded-lg hover:bg-blue-500',
     {
       variants: {
         variant: {
-          primary: 'bg-[#5D37F3]',
-          secondary: 'bg-yellow-600'
+          primary: ' w-full',
+          secondary: 'w-20'
         },
         disabled: {
           true: '!bg-gray-100 !text-gray-400 cursor-not-allowed'
