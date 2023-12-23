4ee93092ec2b09565f5bae4126d1c238e768542f
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-23 23:11:38 +0400

    style: define reusable colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#1A1A1F'
+      }
+    }
   },
   plugins: []
 }
