c75e8e83ce9c10a8ab63c1a88d4c0c4cee7e5619
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-24 17:39:41 +0400

    feat: add auth

--- src/components/shared/LoginForm.vue
+++ src/components/shared/LoginForm.vue
@@ -2,7 +2,7 @@
 import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import { z } from 'zod'
-
+import { useAuthStore } from '@/stores/auth'
 import BaseButton from '@/components/ui/BaseButton.vue'
 
 type ILoginForm = {
@@ -12,26 +12,31 @@ type ILoginForm = {
 const { handleSubmit, errors, defineField } = useForm<ILoginForm>({
   validationSchema: toTypedSchema(
     z.object({
-      email: z.string().min(1).email()
+      email: z.string().min(1).endsWith('@redberry.ge')
     })
   )
 })
 
+const authStore = useAuthStore()
+
 const [email, emailAttrs] = defineField('email')
 
 const onSubmit = handleSubmit((values) => {
-  console.log(values)
+  authStore.getUser(values)
 })
 </script>
 
 <template>
   <form @submit="onSubmit" class="w-full">
-    <div>
+    <div class="mb-6">
+      <label for="email" class="text-primary font-medium text-sm">ელ-ფოსტა</label>
       <input
         v-model="email"
         v-bind="emailAttrs"
         name="email"
-        class="w-full h-11 px-[0.875rem] border border-border-inactive rounded-xl"
+        id="email"
+        placeholder="Example@redberry.ge"
+        class="w-full h-11 px-[1rem] py-3 border border-border-inactive rounded-xl mt-[0.375rem]"
       />
       <div>{{ errors.email }}</div>
     </div>
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,8 +1,17 @@
+import axiosInstance from '@/plugins/axios'
 import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore({
   id: 'auth',
   state: () => ({
-    // authUser = null
-  })
+    isLoggedIn: false
+  }),
+  actions: {
+    async getUser(payload: { email: string }) {
+      const res = await axiosInstance.post(`/login`, payload)
+      if (res.status === 204) {
+        this.isLoggedIn = true
+      }
+    }
+  }
 })
