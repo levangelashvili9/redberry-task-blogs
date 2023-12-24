183036b8d3fc2e59c840b8e77a1241f36d9066a5
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-24 17:54:05 +0400

    feat: add navigation button if authenticated

--- src/components/layouts/default/DefaultHeader.vue
+++ src/components/layouts/default/DefaultHeader.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+
 import BaseButton from '@/components/ui/BaseButton.vue'
 import BaseModal from '@/components/ui/BaseModal.vue'
 import IconRedberry from '@/components/icons/IconRedberry.vue'
@@ -7,12 +10,21 @@ import useModal from '@/composables/useModal'
 import LoginForm from '@/components/shared/LoginForm.vue'
 
 const { isModalActive, openModal, closeModal } = useModal()
+const authStore = useAuthStore()
+const router = useRouter()
+
+const navigateToForm = () => {
+  router.push('/form/createBlog')
+}
 </script>
 
 <template>
   <div class="h-20 flex justify-between items-center px-[4.75rem]">
     <IconRedberry />
-    <BaseButton variant="secondary" @click="openModal">შესვლა</BaseButton>
+    <BaseButton v-if="authStore.isLoggedIn" variant="secondary" @click="navigateToForm"
+      >დაამატე ბლოგი</BaseButton
+    >
+    <BaseButton v-else variant="secondary" @click="openModal">შესვლა</BaseButton>
     <BaseModal :isModalActive="isModalActive" @close-modal="closeModal">
       <h3 class="text-2xl font-bold text-primary">შესვლა</h3>
       <LoginForm />
