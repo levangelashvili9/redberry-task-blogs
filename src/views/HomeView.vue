ba79d8e0d33e4b9d2f0579f2504bb6ad1e59c13f
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-24 16:03:01 +0400

    chore: used reusable color instead of custom

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import HomeHero from '@/components/home/HomeHero.vue'
 </script>
 
 <template>
-  <main class="bg-[#F3F2FA] min-h-screen pt-16">
+  <main class="bg-page-color min-h-screen pt-16">
     <HomeHero />
   </main>
 </template>
