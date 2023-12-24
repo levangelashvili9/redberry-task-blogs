b55db28751df8211b54531dfb06831202e95ea4a
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-24 16:04:44 +0400

    fix: fix multiple rounded property

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -11,7 +11,7 @@ const props = defineProps<IProps>()
 
 const buttonClass = computed(() => {
   return cva(
-    'inline-flex items-center justify-center bg-[#5D37F3] text-white text-sm rounded min-h-10 px-5 py-[0.625rem] font-medium rounded-lg hover:bg-blue-500',
+    'inline-flex items-center justify-center bg-[#5D37F3] text-white text-sm min-h-10 px-5 py-[0.625rem] font-medium rounded-lg hover:bg-blue-500',
     {
       variants: {
         variant: {
