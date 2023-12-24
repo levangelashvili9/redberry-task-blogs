b274c3cf5693eaa1617cba22ddbd6773ef16355e
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-24 21:35:17 +0400

    style: change reusable modal style

--- src/components/ui/BaseModal.vue
+++ src/components/ui/BaseModal.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-// import BaseButton from '@/components/ui/BaseButton.vue'
 import IconCross from '@/components/icons/IconCross.vue'
 
 type IProps = {
@@ -17,13 +16,9 @@ defineEmits(['close-modal'])
       @click="$emit('close-modal')"
       class="fixed bg-black/30 h-screen w-full top-0 left-0 flex justify-center items-center"
     >
-      <div
-        class="bg-white w-[30rem] px-6 py-10 flex flex-col items-center gap-6 relative"
-        @click.stop
-      >
+      <div class="bg-white w-[30rem] px-6 py-10 rounded-xl relative" @click.stop>
         <IconCross class="absolute top-5 right-5 cursor-pointer" @click="$emit('close-modal')" />
         <slot />
-        <!-- <BaseButton variant="primary">შესვლა</BaseButton> -->
       </div>
     </div>
   </teleport>
