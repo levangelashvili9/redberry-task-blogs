fad1e6c2ed13aa17b8964d00b9588ce0c69289ce
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-25 20:56:08 +0400

    wip: add blog filter functionality

---
+++ src/components/home/BlogsFilter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import useFetchCategories from '@/composables/useFetchCategories'
+
+const { categories } = useFetchCategories()
+</script>
+
+<template>
+  <div class="flex justify-center mb-16">
+    <div class="grid grid-cols-5 gap-6 w-[80rem]">
+      <div
+        v-for="category in categories"
+        :key="category.id"
+        class="rounded-[1.875rem] min-h-10 px-4 flex justify-center items-center cursor-pointer hover:opacity-70"
+        :style="{
+          backgroundColor: category.background_color,
+          color: category.text_color
+        }"
+      >
+        <p class="text-sm font-medium">{{ category.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
