df917db5888c5a41048e156bf3f5241a14c062e6
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-25 20:56:43 +0400

    feat: add blog content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import HomeHero from '@/components/home/HomeHero.vue'
+import BlogsFilter from '@/components/home/BlogsFilter.vue'
+import BlogsList from '@/components/home/BlogsList.vue'
 </script>
 
 <template>
   <main class="bg-page-color min-h-screen pt-16">
     <HomeHero />
+    <BlogsFilter />
+    <BlogsList />
   </main>
 </template>
