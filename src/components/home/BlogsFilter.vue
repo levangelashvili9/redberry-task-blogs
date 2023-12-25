8fcf88bf311febc078b0633278e1bb6534bdb4e7
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-25 22:31:57 +0400

    style: change style of filters list

--- src/components/home/BlogsFilter.vue
+++ src/components/home/BlogsFilter.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import useFetchCategories from '@/composables/useFetchCategories'
+import { useFiltersStore } from '@/stores/filters'
 
 const { categories } = useFetchCategories()
+const filtersStore = useFiltersStore()
 </script>
 
 <template>
   <div class="flex justify-center mb-16">
-    <div class="grid grid-cols-5 gap-6 w-[80rem]">
+    <div class="flex justify-center flex-wrap place-items-start gap-6 w-[80rem]">
       <div
         v-for="category in categories"
         :key="category.id"
-        class="rounded-[1.875rem] min-h-10 px-4 flex justify-center items-center cursor-pointer hover:opacity-70"
+        @click="filtersStore.toggleFilter(category.title)"
+        class="rounded-[1.875rem] w-fit min-h-10 px-4 flex justify-center items-center cursor-pointer truncate border-2 hover:opacity-70"
+        :class="{ 'border-black': filtersStore.filters.includes(category.title) }"
         :style="{
           backgroundColor: category.background_color,
           color: category.text_color
