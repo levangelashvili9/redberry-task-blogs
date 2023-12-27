bbda4deeec2c071f059799124a5f0d7c5728a07c
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-27 16:36:52 +0400

    style: change padding bottom

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ import BlogsList from '@/components/home/BlogsList.vue'
 </script>
 
 <template>
-  <main class="bg-page-color min-h-screen pt-16">
+  <main class="bg-page-color min-h-screen pt-16 pb-20">
     <HomeHero />
     <BlogsFilter />
     <BlogsList />
