ae5b85c9906aa750c272b4820998918ca8a18495
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-27 16:39:06 +0400

    refactor: create component for full blog

---
+++ src/components/blog/BlogFull.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useBlogStore } from '@/stores/blog'
+import { useRoute } from 'vue-router'
+import BlogCard from '@/components/shared/BlogCard.vue'
+
+const route = useRoute()
+const blogStore = useBlogStore()
+
+blogStore.fetchSingleBlog(route.params.id)
+const { blog } = storeToRefs(blogStore)
+</script>
+
+<template>
+  <BlogCard v-if="blog" :blog="blog" full />
+</template>
