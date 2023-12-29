ddade622eb2e93d0a702035cf0683fa29824f769
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-12-29 16:15:01 +0400

    style: change create blog page background

--- src/views/CreateBlogView.vue
+++ src/views/CreateBlogView.vue
@@ -4,7 +4,7 @@ import BlogForm from '@/components/create-blog/BlogForm.vue'
 </script>
 
 <template>
-  <main class="bg-page-color min-h-screen pt-16 flex justify-center">
+  <main class="bg-[#FBFAFF] min-h-screen pt-16 pb-32 flex justify-center">
     <div class="w-[37.5rem] flex flex-col">
       <h2 class="text-primary text-[2rem] font-bold mb-10">ბლოგის დამატება</h2>
       <BlogForm />
